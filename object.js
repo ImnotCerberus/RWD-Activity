30cc32f86876542c92aede57979a37464648c3e7
Lazareto, Leandro Renee V. <leandro12renee@gmail.com>
2025-01-18 13:00:23 +0800

    Add files via upload

---
+++ object.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Leandro',
+    age: 19,
+    hobbies: ['games', 'listen music', 'bike',],
+    greet: function(){
+        console.log('Hello');
+    }
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+console.log(person.hobbies[0]);

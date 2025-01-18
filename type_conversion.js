3daa498487a18903fc30a648e9618b17f48fd86d
Lazareto, Leandro Renee V. <leandro12renee@gmail.com>
2025-01-18 12:59:43 +0800

    Add files via upload

---
+++ type_conversion.js
@@ -0,0 +1,8 @@
+val = String(5)
+val = String(4 + 4)
+val = String(true)
+val = String(new Date())
+val = Number('5')
+val = Number(true)
+val = String([1,2,3,4])
+val = (5).toString()
---
+++ window_method.js
@@ -0,0 +1,9 @@
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href= "https://www.youtube.com/watch?v=sElE_BfQ67s";
+val = window.location.search;
+val = window.location.pathname;
+//val = window.location.reload();
+val = window.history;
+console.log(val)
